8dc5b4d629356dbb74eaefd39e3b8edddf9bcdda
ZhouWei <toweizhou@gmail.com>
2014-11-30 23:09:56 +0800

    github line in the helloworld.java

--- GitHelloWorld/src/com/weizhou/git/helloapp/HelloWorld.java
+++ GitHelloWorld/src/com/weizhou/git/helloapp/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello git world!");
+		System.out.println("I have already pulled this proj to the github!");
+		
 	}
 
 }
